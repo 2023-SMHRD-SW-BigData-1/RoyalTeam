be1da347c1c883522fac5aa4315a00ce83734985
Annonamed <ryynmn.r@gmail.com>
2023-11-09 10:49:25 +0900

    마커 추가

---
+++ teamroyal/src/main/java/com/user/vo/MapMarker.java
@@ -0,0 +1,15 @@
+package com.user.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MapMarker {
+    //private String plantCode;  // 발전소 고유 코드
+    private float latitude;    // 위도
+    private float longitude;   // 경도
+
+    
+
+}
